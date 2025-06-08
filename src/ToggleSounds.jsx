f2fa05fba82221b54377048f44d27e462e894fcb
PiusCyre <108563817+PiusCyre@users.noreply.github.com>
2025-06-08 21:13:14 +0100

    Initial Commit

---
+++ src/ToggleSounds.jsx
@@ -0,0 +1,14 @@
+function ToggleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+useMemo;
+// memoize
+export default ToggleSounds;
